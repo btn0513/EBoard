398761ac9a54af7a1aa3252a4c5eb2823fe0de07
Mike <MichaelPhung.nguyen@gmail.com>
2015-04-15 21:27:51 -0500

    fixed profile/fixed Jsonlisting/ fixed details/fixed saved

--- www/js/services.js
+++ www/js/services.js
@@ -51,7 +51,7 @@ angular.module('starter.services', [])
 .factory('Posts', function($http) {
     return {
       getPosts: function() {
-            return $http.get('/js/testingPosts.json');
+            return $http.get('js/testingPosts.json');
             //return $http.get('http://banana-cobbler-6505.herokuapp.com/api/v1/listings.json');
         }
     };
